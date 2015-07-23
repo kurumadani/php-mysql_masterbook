733200de978a64e3776ce4c7e956e0313a4d1319
車谷好弘 <aquamarine_orangeprincess@yahoo.co.jp>
2015-07-23 23:51:32 +0900

    Chapter2 Section11

---
+++ Chapter2/tag3.php
@@ -0,0 +1,14 @@
+	<!-- Section 11 HTMLにPHPを埋め込むには -->
+	<!-- HTMLタグとPHPコードの混在 -->
+	<html>
+	<head>
+		<title>PHPのテスト</title>
+	</head>
+	<body>
+		
+		<?php
+		print "こんにちは！";
+		?>
+
+	</body>
+	</html>
