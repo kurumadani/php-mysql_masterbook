5844771810abdc9ea1244af688ee8a589a35a954
kurumadani <kaka_yoshi01@yahoo.co.jp>
2015-07-23 23:38:03 +0900

    no message

---
+++ Chapter2/tag1.php
@@ -0,0 +1,6 @@
+<?php
+//Section 11 HTMLにPHPを埋め込むには
+//1 PHPコードがある場合
+print "こんにちは！";
+?>
+print "こんにちは！";
---
+++ Chapter2/tag2.php
@@ -0,0 +1,7 @@
+<b>
+<?php
+//Section 11 HTMLにPHPを埋め込むには
+//2 HTMLタグがある場合
+print "こんにちは！";
+?>
+</b>
